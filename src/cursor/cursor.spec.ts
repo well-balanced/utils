f91cb69e96bda883f5718019349cdf257f06c083
Woosik Kim <iam.woosik.kim@gmail.com>
2022-06-24 15:49:01 +0900

    :white_check_mark: Add test for cursor

---
+++ src/cursor/cursor.spec.ts
@@ -0,0 +1,26 @@
+import { encodeCursor, decodeCursor } from '.'
+
+it('cursor', () => {
+  /**
+   * Given
+   */
+  interface User {
+    name: string
+    age: number
+  }
+
+  const user: User = { name: 'name', age: 20 }
+
+  /**
+   * When
+   */
+  const cursor = encodeCursor(user)
+  const decoded = decodeCursor<User>(cursor)
+
+  /**
+   * Then
+   */
+  expect(typeof cursor).toBe('string')
+  expect(decoded.age).toBe(20)
+  expect(decoded.name).toBe('name')
+})

94dd2a13ce19bad3b958e2a50325d8240ae6a204
Woosik Kim <iam.woosik.kim@gmail.com>
2022-06-26 16:04:33 +0900

    :package: Add readdirRecursive

---
+++ src/readdirRecursive/index.ts
@@ -0,0 +1,19 @@
+import path from 'path'
+import fs from 'fs/promises'
+import { reduceSeries } from '../reduceSeries'
+
+export async function readdirRecursive(parent: string): Promise<string[]> {
+  const childs = await fs.readdir(parent)
+  const paths = childs.map((child) => path.join(parent, child))
+  return reduceSeries(
+    paths,
+    async (prev, curr) => {
+      const stat = await fs.stat(curr)
+      return [
+        ...prev,
+        ...(stat.isDirectory() ? await readdirRecursive(curr) : [curr]),
+      ]
+    },
+    [],
+  )
+}
